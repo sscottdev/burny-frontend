214a1ed2a8a7f532d80c6903167bbc7f3b6791b5
jcruz63 <46271776+jcruz63@users.noreply.github.com>
2022-03-08 20:59:00 -0500

    added css module

--- src/components/chat/Chat-AnswerList/Chat-Answer/ChatAnswer.js
+++ src/components/chat/Chat-AnswerList/Chat-Answer/ChatAnswer.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import main from './ChatAnswer.module.css'
+import colors from '../../../../BrandColors.module.css'
+let styles = {};
+Object.assign(styles, main, colors);
 
 export default function ChatAnswer(props) {
   return (
-    <div id={props.id} onClick={props.onClick} data-goto={props.goto}> {props.answer} </div>
+    <button className={styles['blue-grad']} id={props.id} onClick={props.onClick} data-goto={props.goto}> {props.answer} </button>
   )
 }
 
