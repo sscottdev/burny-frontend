d4c615e0dcebcdbf9cc6030a00f1a79c41e9afed
jcruz63 <46271776+jcruz63@users.noreply.github.com>
2022-03-09 07:29:37 -0500

    adding react context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Chat from './components/chat/Chat';
+import MessageContext from './store/chat-context';
 
 function App() {
 
   return (
     <div className="App">
-      <Chat />
+      <MessageContext.Provider value={{currentMessage: "root"}}>
+         <Chat />
+      </MessageContext.Provider>
     </div>
   );
 }
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,33 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import MessageContext from "../../store/chat-context";
 import ChatMessage from "./Chat-Message/ChatMessage";
 import ChatSummary from "./Chat-Summary/ChatSummary";
 import messageService from "./MessageService";
-
 
 export default function Chat(props) {
   // const [chatNode, setChatNode] = useState(messageService.getRoot());
   // const [chatNode, setChatNode] = useState(messageService.goTo("q2"));
 
   const [messages, setMessages] = useState([messageService.getRoot()]);
-
+  const ctx = useContext(MessageContext);
   const onSelectionHandler = (event) => {
-    console.log(event.target.dataset.goto)
-    setMessages(prevMessages => {
-      return [...prevMessages, messageService.goTo(event.target.dataset.goto) ]
-    })
-}
+    console.log(event.target.dataset.goto);
+    setMessages((prevMessages) => {
+      return [...prevMessages, messageService.goTo(event.target.dataset.goto)];
+    });
+  };
 
   return (
-    <div>
-      {
-        messages.map(message => {
-          if(message.type === "question"){
-            return <ChatMessage key={message.id} message={message} onSelection={onSelectionHandler}/>
-          } else if(message.type === "summary"){
-            return <ChatSummary key={message.id} summary={message.val}/>
-          }
-        })
-      }
-    </div>
+    <React.Fragment>
+      {messages.map((message) => {
+        if (message.type === "question") {
+          console.log(ctx);
+          return (
+            <ChatMessage
+              key={message.id}
+              message={message}
+              onSelection={onSelectionHandler}
+            />
+          );
+        } else if (message.type === "summary") {
+          return <ChatSummary key={message.id} summary={message.val} />;
+        }
+      })}
+    </React.Fragment>
+    // <div>
+    //   {
+    //     messages.map(message => {
+    //       if(message.type === "question"){
+    //         console.log(message)
+    //         return <ChatMessage key={message.id} message={message} onSelection={onSelectionHandler}/>
+    //       } else if(message.type === "summary"){
+    //         return <ChatSummary key={message.id} summary={message.val}/>
+    //       }
+    //     })
+    //   }
+    // </div>
   );
 }
