2fa6ededa7fed1eb425410234ec6ffb388f01646
VogueFxxtr <voguefxxtr360@gmail.com>
2022-03-10 09:48:43 -0500

    Changed grammer and changed logo image in summary

--- src/components/chat/Chat-Summary/ChatSummary.js
+++ src/components/chat/Chat-Summary/ChatSummary.js
@@ -6,7 +6,7 @@ export default function ChatSummary(props) {
     <div>
       <h2>Summary</h2>
       <div>
-      <img src='https://via.placeholder.com/50' alt='burny chat logo'/>
+      <img src='https://cdn.discordapp.com/attachments/939185200238305330/946815947946213426/Untitled-1.png' alt='burny chat logo'/>
       <p>
           {props.summary}
       </p>
